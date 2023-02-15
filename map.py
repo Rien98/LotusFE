e71dfcb68853ad7a9b8049cf68e644fc5ff9f41b
DatamanTheGreat <39222406+DatamanTheGreat@users.noreply.github.com>
2023-02-15 17:59:55 -0600

    changed leaflet api from folium to dash

--- map.py
+++ map.py
@@ -1,39 +1,26 @@
-
-import folium
-from folium.plugins import LocateControl
+import json
+# install dash with pip install -U dash
+import dash
+import dash_leaflet as dl
+import dash_html_components as html
+from dash.dependencies import Input, Output, ALL
 import webbrowser
-import cgitb 
-cgitb.enable()
 
-# centers map on simrall
-m0 = folium.Map(width="50%", location=[33.4526221, -88.7872477], zoom_start=100)
-LocateControl().add_to(m0)
-folium.ClickForLatLng().add_to(m0)
-m0.save("map_index.html")
+# create map object
+map = dash.Dash()
+map.layout = dl.Map(dl.TileLayer(),center=[33.4526221, -88.7872477], zoom=100, style={'width': '1000px', 'height': '500px'})
 
+# Callback for interactivity.
+@map.callback(Output("log", "children"), Input(dict(id=ALL), "n_clicks"))
+def log_position(_):
+    idx = json.loads(dash.callback_context.triggered[0]['prop_id'].split(".")[0])["id"]
+    location = locations[idx]
+    print(location)  # print location to console
+    return location  # print location to ui
 
 # modify html here
 
-
-
-# open web browser
-webbrowser.open_new_tab('map_index.html')
-
-#x an y coord
-x1 = float(input("Enter The Starting Position North:"))
-x2 = float(input("Enter The Starting Position West:"))
-y1 = float(input("Enter The Ending Position North:"))
-y2 = float(input("Enter The Starting Position West:"))
-x = (x1, x2)
-y = (y1, y2)
-
-m = folium.Map(location = [x1,x2],zoom_start=17) #creating masking layer via direct
-LocateControl().add_to(m)
-loc =[x,y]
-folium.LatLngPopup().add_to(m)
-folium.Marker([x1,x2],tooltip="<i>Starting Position</i>").add_to(m)
-folium.Marker([y1,y2],tooltip="<i>Ending Position</i>").add_to(m)
-folium.PolyLine(loc,color='blue', weight=10).add_to(m)
-
-m.save("map_index.html")
-webbrowser.open_new_tab('map_index.html')
+# run server and open web browser
+if __name__ == '__main__':
+    map.run_server()
+webbrowser.open_new_tab("http://localhost:8050")
