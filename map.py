9b675103f66af0b22d8bbc1349da7207eb8bdb3b
DatamanTheGreat <39222406+DatamanTheGreat@users.noreply.github.com>
2023-02-15 21:41:42 -0600

    lat longs are saved to waypoints vector. also html header change

--- map.py
+++ map.py
@@ -2,25 +2,28 @@ import json
 # install dash with pip install -U dash
 import dash
 import dash_leaflet as dl
-import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output, ALL
-import webbrowser
 
-# create map object
-map = dash.Dash()
-map.layout = dl.Map(dl.TileLayer(),center=[33.4526221, -88.7872477], zoom=100, style={'width': '1000px', 'height': '500px'})
+# create front end
+FE = dash.Dash(prevent_initial_callbacks=True)
+FE.layout = html.Div([
+    html.H1("L.O.T.U.S. Dashboard"),
+    dl.Map([dl.TileLayer(), dl.LayerGroup(id="layer")],
+           id="map", style={'width': '1000px', 'height': '50vh', 'margin': "auto", "display": "block"}, center=[33.4526221, -88.7872477], zoom=100),
+])
 
-# Callback for interactivity.
-@map.callback(Output("log", "children"), Input(dict(id=ALL), "n_clicks"))
-def log_position(_):
-    idx = json.loads(dash.callback_context.triggered[0]['prop_id'].split(".")[0])["id"]
-    location = locations[idx]
-    print(location)  # print location to console
-    return location  # print location to ui
+# initialize locations
+waypoints = []
 
-# modify html here
+# store click lat longs
+@FE.callback(Output("layer", "children"), [Input("map", "click_lat_lng")])
+def map_click(click_lat_lng):
+    location = (click_lat_lng)
+    waypoints.append("{:.8f},{:.8f}".format(*click_lat_lng))
+    #print(locations)
+    return [dl.Marker(position=click_lat_lng, children=dl.Tooltip("({:.3f}, {:.3f})".format(*click_lat_lng)))]
 
-# run server and open web browser
+# run server
 if __name__ == '__main__':
-    map.run_server()
-webbrowser.open_new_tab("http://localhost:8050")
+    FE.run_server()
